6d2fd3c054def18ddf066590a7310486b876aa25
jackgreff <112905511+jackgreff@users.noreply.github.com>
2023-01-23 01:33:34 -0500

    Delete VariousExercises/src directory

--- VariousExercises/src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String args[])
-    {
-        System.out.println("Use this project for things that don't need to be submitted,");
-        System.out.println("  such as class exercises, homework. etc.");
-    }
-}
